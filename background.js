e4e357bdd74c65142b78ca8a0b7bab6957dad029
enterthestream <zaid9010@gmail.com>
2025-05-21 16:37:00 +0100

    WIP: context menu item for adding film to Letterboxd watchlist

--- background.js
+++ background.js
@@ -1,30 +1,65 @@
 import fetchFilmData from "./api.js";
 
+// adds context menu item for adding film to Letterboxd watchlist
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "addToLetterboxdWatchlist",
+    title: "CineMenu: Add to Letterboxd watchlist",
+    contexts: ["page", "selection"],
+  });
+});
+
+// access local storage for last film hovered
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "addToLetterboxdWatchlist") {
+    chrome.storage.get("lastFilmHovered", (result) => {
+      result.lastFilmHovered;
+    });
+    if (film && film.Title && film.TMDBid) {
+      chrome.scripting.executeScript({
+        target: { tab: tab.id },
+        func: (text) => {
+          alert(text);
+        },
+        args: [film.TMDBid],
+      });
+    } else {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => alert("No hovered film found"),
+      });
+    }
+  }
+});
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "fetchFilmData") {
     fetchFilmData(message.title)
       .then((data) => {
-        if(data.Response === "True") {
+        if (data.Response === "True") {
           // call list of films or initialise
-          chrome.storage.local.get("FilmsHovered", (result) => {
-            const filmsList = result.FilmsHovered || []
+          chrome.storage.local.get("filmsHovered", (result) => {
+            const filmsList = result.filmsHovered || [];
 
-            const alreadyExists = filmsList.some(f => f.imdbID === data.imdbID)
-            if(!alreadyExists){
-              filmsList.push(data)
+            const alreadyExists = filmsList.some(
+              (f) => f.imdbID === data.imdbID
+            );
+            if (!alreadyExists) {
+              filmsList.push(data);
             }
 
-            const trimmedFilmsList = filmsList.slice(-20)
-        // store last hovered title and new filmsList
-            chrome.storage.local.set({
-              lastFilmHovered: data,
-              FilmsHovered: trimmedFilmsList}, () => {
-                              console.log("Saved film data and updated FilmsHovered list")
+            const trimmedFilmsList = filmsList.slice(-20);
+            // store last hovered title and new filmsList
+            chrome.storage.local.set(
+              {
+                lastFilmHovered: data,
+                filmsHovered: trimmedFilmsList,
+              },
+              () => {
+                console.log("Saved film data and updated filmsHovered list");
               }
-
-            )
-          })
-
+            );
+          });
         }
         sendResponse({ data });
       })
